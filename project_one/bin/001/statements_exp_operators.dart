fc39d4f5c9c20560e3c8cba671140fea8b140253
renankanu <renankanu@gmail.com>
2021-09-01 14:29:00 -0300

    001 - statements, expressions and Arithmetic operations

---
+++ project_one/bin/001/statements_exp_operators.dart
@@ -0,0 +1,19 @@
+void main() {
+  /// Statements
+  print('Hello World!');
+  print('Hello World Two!');
+
+  /// Expressions
+  42;
+  3 + 2;
+  'Hello, Dart Apprentice reader!';
+
+  /// Arithmetic operations
+  print(1 + 1);
+  print(2 - 1);
+  print(2 * 2);
+  print(3 / 2);
+
+  /// Integer division
+  print(4 ~/ 3);
+}
