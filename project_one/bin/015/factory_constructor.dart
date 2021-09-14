fb9d6873f981776dea775e1d107f5244ed4fa10f
renankanu <renankanu@gmail.com>
2021-09-13 23:50:59 -0300

    create factory constructor

---
+++ project_one/bin/015/factory_constructor.dart
@@ -0,0 +1,15 @@
+class Car {
+  String make;
+  String bodyType;
+
+  Car({this.make = '', this.bodyType = 'Truck'});
+
+  factory Car.suv() {
+    return Car(bodyType: 'SUV');
+  }
+}
+
+void main() {
+  var carSuv = Car.suv();
+  print(carSuv.bodyType);
+}
